0a0c16f78c44f6cb27050740f06f841eb0fceaaa
Maxim Sklyarov <Maxim_Sklyarov@dell.com>
2021-12-03 11:01:25 +0300

    initial replication code

---
+++ api/v11/api_v11_replication.go
@@ -0,0 +1,55 @@
+package v11
+
+import (
+	"context"
+	"github.com/dell/goisilon/api"
+)
+
+const (
+	policiesPath = "/platform/11/sync/policies/"
+)
+
+// Policy contains the CloudIQ policy info.
+type Policy struct {
+	Action       string `json:"action,omitempty"`
+	Id           string `json:"id,omitempty"`
+	Name         string `json:"name,omitempty"`
+	Enabled      bool   `json:"enabled,omitempty"`
+	TargetPath   string `json:"target_path,omitempty"`
+	SourcePath   string `json:"source_root_path,omitempty"`
+	TargetHost   string `json:"target_host,omitempty"`
+	JobDelay     int    `json:"job_delay,omitempty"`
+	Schedule     string `json:"schedule,omitempty"`
+}
+
+type Policies struct {
+	Policy []Policy `json:"policies,omitempty"`
+}
+
+// GetPolicyByName returns policy by name
+func GetPolicyByName(
+	ctx context.Context,
+	client api.Client, name string) (policy *Policy, err error) {
+	p := &Policies{}
+	err = client.Get(ctx, policiesPath, name, nil, nil, &p)
+	if err != nil || len(p.Policy) == 0 {
+		return nil, err
+	}
+		return &p.Policy[0], nil
+}
+
+func CreatePolicy(ctx context.Context, client api.Client, name string, sourcePath string, targetPath string, targetHost string, rpo int) (error) {
+	resp := ""
+	body := &Policy{
+		Action:     "sync",
+		Id:         "",
+		Name:       name,
+		Enabled:    true,
+		TargetPath: targetPath,
+		SourcePath: sourcePath,
+		TargetHost: targetHost,
+		JobDelay:   rpo,
+		Schedule:   "when-source-modified",
+	}
+	return client.Post(ctx,policiesPath, "", nil,nil,body,resp)
+}
