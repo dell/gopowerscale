7bcc578929cb154c62161ddc4ddd1362cb0f4c4a
Andrey Schipilo <Andrey.Schipilo@emc.com>
2022-02-24 12:46:48 +0300

    Addressed PR comments

--- api/v11/api_v11_replication.go
+++ api/v11/api_v11_replication.go
@@ -1,3 +1,18 @@
+/*
+ Copyright (c) 2022 Dell Inc, or its subsidiaries.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 package v11
 
 import (
@@ -191,8 +206,11 @@ func StartSyncIQJob(ctx context.Context, client api.Client, job *JobRequest) (*J
 func GetReport(ctx context.Context, client api.Client, reportName string) (*Report, error) {
 	r := &Reports{}
 	err := client.Get(ctx, reportsPath, reportName, nil, nil, &r)
-	if err != nil || len(r.Reports) == 0 {
+	if err != nil {
 		return nil, err
+	}
+	if len(r.Reports) == 0 {
+		return nil, fmt.Errorf("no reports found with report name %s", reportName)
 	}
 	return &r.Reports[0], nil
 }
