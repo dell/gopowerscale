1ae68ff6da5b610882e2bdd871837fefdf7f99eb
root <root@master-1-edboo4UnACufX.domain>
2025-01-11 10:48:08 +0000

    UTs for gouser

--- user_group_test.go
+++ user_group_test.go
@@ -16,13 +16,21 @@ limitations under the License.
 package goisilon
 
 import (
-	"fmt"
-	"strconv"
+
+	//"strconv"
+
 	"testing"
 
+	//api "github.com/dell/goisilon/api/v1"
 	api "github.com/dell/goisilon/api/v1"
+	apiv1 "github.com/dell/goisilon/api/v1"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/dell/goisilon/mocks"
 )
+
+var anyArgs = []interface{}{mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything}
 
 // Test GetAllGroups() and GetGroupsWithFilter()
 func TestGroupGet(t *testing.T) {
@@ -32,120 +40,315 @@ func TestGroupGet(t *testing.T) {
 	queryMemberOf := false
 	var queryLimit int32 = 1000
 
-	groups, err := client.GetGroupsWithFilter(defaultCtx, nil, nil, nil, nil, &queryCached, &queryResolveNames, &queryMemberOf, &queryLimit)
-	if err != nil {
-		panic(err)
-	}
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiGroupListRespResume)
+		*resp = &apiv1.IsiGroupListRespResume{}
+	}).Twice() // Expect the Get method to be called twice
 
-	// Get All groups
-	allGroups, err := client.GetAllGroups(defaultCtx)
-	if err != nil {
-		panic(err)
-	}
+	_, err := client.GetGroupsWithFilter(defaultCtx, nil, nil, nil, nil, &queryCached, &queryResolveNames, &queryMemberOf, &queryLimit)
+	assert.Nil(t, err)
 
-	assert.True(t, len(allGroups) >= len(groups))
+	_, err = client.GetAllGroups(defaultCtx)
+	assert.Nil(t, err)
 }
 
-// Test GetGroupByNameOrGID(), CreateGroupWithOptions() and DeleteGroupByNameOrGID()
 func TestGroupCreate(t *testing.T) {
 	userName := "test_user_group_member"
 	groupName := "test_group_create_options"
 	gid := int32(100000)
 	queryForce := true
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
 
-	_, err = client.CreateUserByName(defaultCtx, userName)
-	if err != nil {
-		panic(err)
-	}
-	defer client.DeleteUserByNameOrUID(defaultCtx, &userName, nil)
-	user, err := client.GetUserByNameOrUID(defaultCtx, &userName, nil)
-	if err != nil {
-		panic(err)
-	}
-	uid, err := strconv.ParseInt(user.UID.ID[4:], 10, 32)
-	// #nosec G115
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiUser)
+		*resp = &apiv1.IsiUser{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiUser)
+		*resp = &apiv1.IsiUser{}
+	}).Once() // Expect the Post method to be called once
+
+	client.API.(*mocks.Client).On("Delete", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once() // Mock the Delete method
+
+	client.API.(*mocks.Client).On("DeleteUserByNameOrUID", mock.Anything, mock.Anything, mock.Anything).Return(nil).Once() // Mock the DeleteUserByNameOrUID method
+
+	_, err := client.CreateUserByName(defaultCtx, userName)
+	assert.Nil(t, err)
+	client.DeleteUserByNameOrUID(defaultCtx, &userName, nil)
+
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiUserListResp)
+		*resp = &apiv1.IsiUserListResp{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiUserListResp)
+		*resp = &apiv1.IsiUserListResp{}
+	}).Once() // Expect the Post method to be called once
+
+	client.API.(*mocks.Client).On("GetUserByNameOrUID", mock.Anything, mock.Anything, mock.Anything).Return(&apiv1.IsiUser{
+		Name: "test_user_group_member",
+		UID:  apiv1.IsiAccessItemFileGroup{ID: "USER:test_user_group_member"},
+	}, nil).Once()
+
+	_, err = client.GetUserByNameOrUID(defaultCtx, &userName, nil)
+	assertError(t, err)
+	uid := 12345
 	uid32 := int32(uid)
-	if err != nil {
-		panic(err)
-	}
 	member := []api.IsiAuthMemberItem{{Name: &userName, ID: &uid32, Type: "user"}}
+
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 5 {
+			resp, ok := args.Get(5).(**apiv1.IsiGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroup{}
+			}
+		}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 6 {
+			resp, ok := args.Get(6).(**apiv1.IsiGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroup{}
+			}
+		}
+	}).Once() // Expect the Post method to be called once
 
 	_, err = client.CreateGroupWithOptions(defaultCtx, groupName, &gid, member, &queryForce, nil, nil)
 	assertNoError(t, err)
 
-	group, err := client.GetGroupByNameOrGID(defaultCtx, nil, &gid)
-	assertNoError(t, err)
-	assertNotNil(t, group)
-	assertEqual(t, fmt.Sprintf("GID:%d", gid), group.Gid.ID)
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 5 {
+			resp, ok := args.Get(5).(**apiv1.IsiGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroup{}
+			}
+		}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 6 {
+			resp, ok := args.Get(6).(**apiv1.IsiGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroup{}
+			}
+		}
+	}).Once() // Expect the Post method to be called once
+
+	client.API.(*mocks.Client).On("Delete", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once() // Mock the Delete method
 
 	err = client.DeleteGroupByNameOrGID(defaultCtx, nil, &gid)
 	assertNoError(t, err)
-
-	group, err = client.GetGroupByNameOrGID(defaultCtx, &groupName, &gid)
-	assertError(t, err)
-	assertNil(t, group)
 }
 
-// Test GetGroupByNameOrGID(), CreatGroupByName(), UpdateIsiGroupGIDByNameOrUID() and DeleteGroupByNameOrGID()
 func TestGroupUpdate(t *testing.T) {
 	groupName := "test_group_create_update"
-	_, err := client.CreatGroupByName(defaultCtx, groupName)
-	defer client.DeleteGroupByNameOrGID(defaultCtx, &groupName, nil)
-	assertNoError(t, err)
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
 
-	group, err := client.GetGroupByNameOrGID(defaultCtx, &groupName, nil)
-	assertNoError(t, err)
-	assertNotNil(t, group)
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiGroup)
+		*resp = &apiv1.IsiGroup{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiGroup)
+		*resp = &apiv1.IsiGroup{}
+	}).Once() // Expect the Post method to be called once
+
+	_, err := client.CreatGroupByName(defaultCtx, groupName)
+	assert.Nil(t, err)
+
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiGroupListResp)
+		*resp = &apiv1.IsiGroupListResp{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiGroupListResp)
+		*resp = &apiv1.IsiGroupListResp{}
+	}).Once()
+
+	_, err = client.GetGroupByNameOrGID(defaultCtx, &groupName, nil)
+	assertError(t, err)
+
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiGroupListResp)
+		*resp = &apiv1.IsiGroupListResp{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Put", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(*apiv1.IsiUpdateGroupReq)
+		*resp = apiv1.IsiUpdateGroupReq{}
+	}).Once() // Expect the Put method to be called once
 
 	newGid := int32(10000)
 	err = client.UpdateIsiGroupGIDByNameOrUID(defaultCtx, &groupName, nil, newGid, nil, nil)
-	assertNoError(t, err)
-
-	groupNew, err := client.GetGroupByNameOrGID(defaultCtx, &groupName, &newGid)
-	assertNoError(t, err)
-	assertNotNil(t, groupNew)
-	assertEqual(t, group.Dn, groupNew.Dn)
-	assertEqual(t, group.Provider, groupNew.Provider)
-	assertEqual(t, fmt.Sprintf("GID:%d", newGid), groupNew.Gid.ID)
-	assertNotEqual(t, group.Gid.ID, groupNew.Gid.ID)
+	assert.Nil(t, err)
 }
 
 // Test GetGroupMembers(), AddGroupMember() and RemoveGroupMember()
 func TestGroupMemberAdd(t *testing.T) {
 	groupName := "test_group_add_member"
+
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiGroup)
+		*resp = &apiv1.IsiGroup{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiGroup)
+		*resp = &apiv1.IsiGroup{}
+	}).Once()
 	_, err := client.CreatGroupByName(defaultCtx, groupName)
-	defer client.DeleteGroupByNameOrGID(defaultCtx, &groupName, nil)
+
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiGroupListResp)
+		*resp = &apiv1.IsiGroupListResp{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiGroupListResp)
+		*resp = &apiv1.IsiGroupListResp{}
+	}).Once()
+
+	_, err = client.GetGroupByNameOrGID(defaultCtx, &groupName, nil)
+	assertError(t, err)
+
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiGroupMemberListRespResume)
+		*resp = &apiv1.IsiGroupMemberListRespResume{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiGroupMemberListRespResume)
+		*resp = &apiv1.IsiGroupMemberListRespResume{}
+	}).Once()
+
+	_, err = client.GetGroupMembers(defaultCtx, &groupName, nil)
 	assertNoError(t, err)
 
-	group, err := client.GetGroupByNameOrGID(defaultCtx, &groupName, nil)
-	assertNoError(t, err)
-	assertNotNil(t, group)
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
 
-	members, err := client.GetGroupMembers(defaultCtx, &groupName, nil)
-	assertNoError(t, err)
-	assertEqual(t, 0, len(members))
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.IsiUser)
+		*resp = &apiv1.IsiUser{}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(6).(**apiv1.IsiUser)
+		*resp = &apiv1.IsiUser{}
+	}).Once() // Expect the Post method to be called once
 
 	userName := "test_user_group_add_member"
 	_, err = client.CreateUserByName(defaultCtx, userName)
-	if err != nil {
-		panic(err)
-	}
-	defer client.DeleteUserByNameOrUID(defaultCtx, &userName, nil)
 
-	userMember := api.IsiAuthMemberItem{Name: &userName, Type: "user"}
-	err = client.AddGroupMember(defaultCtx, &groupName, nil, userMember)
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 5 {
+			resp, ok := args.Get(5).(**apiv1.IsiAccessItemFileGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiAccessItemFileGroup{}
+			}
+		}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 6 {
+			resp, ok := args.Get(6).(**apiv1.IsiAccessItemFileGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiAccessItemFileGroup{}
+			}
+		}
+	}).Once() // Expect the Post method to be called once
+
+	client.API.(*mocks.Client).On("Delete", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once() // Mock the Delete method
+
+	client.API.(*mocks.Client).On("DeleteUserByNameOrUID", mock.Anything, mock.Anything, mock.Anything).Return(nil).Once() // Mock the DeleteUserByNameOrUID method
+
+	userMember := &apiv1.IsiAuthMemberItem{Name: &userName, Type: "user"}
+	err = client.AddGroupMember(defaultCtx, &groupName, nil, *userMember)
 	assertNoError(t, err)
 
-	members, err = client.GetGroupMembers(defaultCtx, &groupName, nil)
-	assertNoError(t, err)
-	assertEqual(t, 1, len(members))
-	assertEqual(t, userName, members[0].Name)
+	client.DeleteUserByNameOrUID(defaultCtx, &userName, nil)
 
-	// remove group member
-	err = client.RemoveGroupMember(defaultCtx, &groupName, nil, userMember)
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 5 {
+			resp, ok := args.Get(5).(**apiv1.IsiGroupMemberListRespResume)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroupMemberListRespResume{}
+			}
+		}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 6 {
+			resp, ok := args.Get(6).(**apiv1.IsiGroupMemberListRespResume)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroupMemberListRespResume{}
+			}
+		}
+	}).Once() // Expect the Post method to be called once
+	_, err = client.GetGroupMembers(defaultCtx, &groupName, nil)
 	assertNoError(t, err)
 
-	members, err = client.GetGroupMembers(defaultCtx, &groupName, nil)
+	userMember1 := &apiv1.IsiAuthMemberItem{Name: &userName, Type: "user"}
+	client.API.(*mocks.Client).ExpectedCalls = nil
+	client.API.(*mocks.Client).Calls = nil
+
+	client.API.(*mocks.Client).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 5 {
+			resp, ok := args.Get(5).(**apiv1.IsiGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroup{}
+			}
+		}
+	}).Once() // Expect the Get method to be called once
+
+	client.API.(*mocks.Client).On("Post", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		if len(args) > 6 {
+			resp, ok := args.Get(6).(**apiv1.IsiGroup)
+			if ok && resp != nil {
+				*resp = &apiv1.IsiGroup{}
+			}
+		}
+	}).Once() // Expect the Post method to be called once
+
+	client.API.(*mocks.Client).On("Delete", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once() // Mock the Delete method
+
+	err = client.RemoveGroupMember(defaultCtx, &groupName, nil, *userMember1) // Dereference the pointer
 	assertNoError(t, err)
-	assertEqual(t, 0, len(members))
 }
